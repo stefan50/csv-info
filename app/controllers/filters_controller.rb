878a3099ec30bc88096c8b5f7e99d71b4073d659
stefan50 <ste.angelov.tues19@gmail.com>
2017-10-09 13:05:45 -0400

    Filters done

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,5 +1,14 @@
 class FiltersController < ApplicationController
 	def create
-		
+		file = params[:file]
+                file_path = file.path
+                result = 0
+                CSV.foreach(file_path) do |row|
+			if row[2].to_f%2 != 0
+				result += row[1].to_f
+			end
+                end
+                result = result.ceil
+                render plain: "%.2f"%result		
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sums' => 'sums#index'
   post 'sums' => 'sums#create'
+  post 'filters' => 'filters#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
