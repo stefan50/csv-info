d17af9a930a9911b38aa380082ad765e93440746
stefan50 <ste.angelov.tues19@gmail.com>
2017-10-09 11:31:59 -0400

    demo

--- app/controllers/sums_controller.rb
+++ app/controllers/sums_controller.rb
@@ -1,9 +1,15 @@
+require 'csv'
 class SumsController < ApplicationController
 	def index
 
 	end
 	def create
-		#file = params[:file]
-		
+		file = params[:file]
+		file_path = file.path
+		result = 0
+		CSV.foreach(file_path) do |row|
+			result += row[0].to_f
+		end
+		render plain: result
 	end
 end
