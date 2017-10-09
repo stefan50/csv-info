b5326259bbc4a20cb316d3dfa9b4c49ab5d40264
stefan50 <ste.angelov.tues19@gmail.com>
2017-10-09 11:03:24 -0400

    hello

---
+++ app/controllers/sums_controller.rb
@@ -0,0 +1,5 @@
+class SumsController < ApplicationController
+	def index
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'sums/index'
+  root 'sums#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
