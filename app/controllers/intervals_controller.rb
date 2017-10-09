754150ae7af6fa445e4453f23a94da3dc1994a1b
stefan50 <ste.angelov.tues19@gmail.com>
2017-10-09 15:13:24 -0400

    Intervals done v2

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -3,19 +3,23 @@ class IntervalsController < ApplicationController
         def create
                 file = params[:file]
                 file_path = file.path
-                result = 0
 		max = 0
 		num = 0
-		for i in num..(num+30)
+		num_of_rows = 0
+		CSV.foreach(file_path) do |row|
+			num_of_rows += 1 
+		end
+		while (num+30)<=num_of_rows
+			result = 0
                 	CSV.foreach(file_path) do |row|
-					result += row[0].to_f
-					if num == 0
-						max = result
-					else
-						if max < result
-							max = result
-						end
-					end
+				result += row[0].to_f
+			end
+			if num == 0
+				max = result
+			else
+				if max < result
+					max = result
+				end
 			end
                 end
                 max = max.ceil
