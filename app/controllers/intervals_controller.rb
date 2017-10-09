e4c4e438560e90a58c89f2befd4a23005cd7747a
stefan50 <ste.angelov.tues19@gmail.com>
2017-10-09 14:45:00 -0400

    Test intervals as sums

---
+++ app/controllers/intervals_controller.rb
@@ -0,0 +1,14 @@
+class IntervalsController < ApplicationController
+	protect_from_forgery except: :create
+        def create
+                file = params[:file]
+                file_path = file.path
+                result = 0
+                CSV.foreach(file_path) do |row|
+                        result += row[0].to_f
+                end
+                result = result.ceil
+                render plain: "%.2f"%result
+        end
+
+end
