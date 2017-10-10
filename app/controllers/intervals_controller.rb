7df4b73c2a9f7151568b066a78c5d9ce47d9801f
stefan50 <ste.angelov.tues19@gmail.com>
2017-10-10 04:17:35 -0400

    Num of rows is working?

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -5,28 +5,31 @@ class IntervalsController < ApplicationController
                 file_path = file.path
 		max = 0
 		num = 0
+		num_of_rows = 0
 		CSV.foreach(file_path) do |row|
 			num_of_rows += 1
 		end
-		while (num+30)<=num_of_rows
-			result = 0
-                	CSV.foreach(file_path).with_index(1) do |row,which_row|
-				if num+29 == which_row
-					break
+		=begin
+			while (num+30)<=num_of_rows
+				result = 0
+                		CSV.foreach(file_path).with_index(1) do |row,which_row|
+					if num+29 == which_row
+						break
+					end
+					result += row[0].to_f
 				end
-				result += row[0].to_f
-			end
-			if num == 0
-				max = result.to_f
-			else
-				if max < result
+				if num == 0
 					max = result.to_f
+				else
+					if max < result
+						max = result.to_f
+					end
 				end
-			end
-			num += 1
-                end
-                max = max.ceil
-                render plain: "%.2f"%max
+				num += 1
+                	end
+		=end
+                #max = max.ceil
+                render plain: num_of_rows
         end
 
 end
