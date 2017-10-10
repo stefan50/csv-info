360c626c9f411c2de1539df12f833eac7684596a
stefan50 <ste.angelov.tues19@gmail.com>
2017-10-10 04:28:20 -0400

    Fix

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -9,27 +9,25 @@ class IntervalsController < ApplicationController
 		CSV.foreach(file_path) do |row|
 			num_of_rows += 1
 		end
-		=begin
-			while (num+30)<=num_of_rows
-				result = 0
-                		CSV.foreach(file_path).with_index(1) do |row,which_row|
-					if num+29 == which_row
-						break
-					end
-					result += row[0].to_f
+		while (num+30)<=num_of_rows
+			result = 0
+               		CSV.foreach(file_path).with_index(1) do |row,which_row|
+				if num+29 == which_row
+					break
 				end
-				if num == 0
+				result += row[0].to_f
+			end
+			if num == 0
+				max = result.to_f
+			else
+				if max < result
 					max = result.to_f
-				else
-					if max < result
-						max = result.to_f
-					end
 				end
-				num += 1
-                	end
-		=end
-                #max = max.ceil
-                render plain: "%d"%num_of_rows
+			end
+			num += 1
+               	end
+                max = max.ceil
+                render plain: "%.2f"%max
         end
 
 end
