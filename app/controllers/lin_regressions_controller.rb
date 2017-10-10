af56b16723ee10267a66daf888c382a97e319ff1
stefan50 <ste.angelov.tues19@gmail.com>
2017-10-10 08:48:14 -0400

    Lin regression

--- app/controllers/lin_regressions_controller.rb
+++ app/controllers/lin_regressions_controller.rb
@@ -1,4 +1,15 @@
+require 'csv'
+require 'regression.rb'
 class LinRegressionsController < ApplicationController
+	protect_from_forgery except: :create	
 	def create
+		file = params[:file]
+		file_path = file.path
+		result = 0
+		CSV.foreach(file_path) do |row|
+			result += row[0].to_f
+		end
+		result = result.ceil
+		render plain: "%.2f"%result 
 	end
 end
